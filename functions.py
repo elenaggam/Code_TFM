aba4d971ef6ecc99e466032214a0af951d3539dd
Elena <839557@unizar.es>
2025-11-29 16:39:07 +0100

    naive background removal

--- functions.py
+++ functions.py
@@ -1,11 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import hyperspy.api as hs
+from scipy.interpolate import interp1d
 
 
 # Background substraction for EELS spectra
+def background_first_step(s, x, y, E=None):
+    """
+    First step done for background removal in EELS spectra.
+    Checks the validity of the input parameters.
+    Selects roi and energy range.
 
+    Parameters:
+    -----------
+    s : hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The input (EELS) spectrum.
+    x : tuple
+        The range in the x-direction to consider for background calculation.
+    y : tuple
+        The range in the y-direction to consider for background calculation.
+    E : tuple (optional)
+        The energy range to consider for background calculation in eV.
 
+    Returns:
+    --------
+    averaged_pixels : np.ndarray
+        The averaged spectrum over the selected region.
+    small_s : hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The data of interest for background calculation.
+    """
+
+    # Area check
+    if x[0] >= x[1] or y[0] >= y[1] or np.any(np.array(x) < 0) or np.any(np.array(y) < 0):
+        raise ValueError("\nIn background_constant: invalid ROI dimensions\n")
+    
+    roi = hs.roi.RectangularROI(left=x[0], right=x[1], top=y[0], bottom=y[1])
+    small_s = roi(s)
+
+    # Energy range check
+    if E is not None:
+        if E[0] >= E[1] or E[0] < small_s.axes_manager[2].axis[0] or E[1] > small_s.axes_manager[2].axis[-1]:
+            raise ValueError("\nIn background_polyfit: invalid energy range\n")
+        small_s = small_s.isig[E[0]:E[1]]
+
+    # Pixels are averaged to get a single spectrum called background
+    averaged_pixels = np.mean(small_s.data, axis=(0,1))
+
+    return averaged_pixels, small_s
 
 def background_constant(s, x, y, E=None):
     """
@@ -21,7 +62,7 @@ def background_constant(s, x, y, E=None):
     y : tuple
         The range in the y-direction to consider for background calculation.
     E : tuple (optional)
-        The energy range to consider for background calculation.
+        The energy range to consider for background calculation in eV.
 
     Returns:
     --------
@@ -29,27 +70,14 @@ def background_constant(s, x, y, E=None):
         The background-subtracted EELS spectrum.
     """
 
-    # Area check
-    if x[0] >= x[1] or y[0] >= y[1] or np.any(np.array(x) < 0) or np.any(np.array(y) < 0):
-        raise ValueError("\nIn background_constant: invalid ROI dimensions\n")
+    background, small_s = background_first_step(s, x, y, E)
     
-    roi = hs.roi.RectangularROI(left=x[0], right=x[1], top=y[0], bottom=y[1])
-    small_s = roi(s)
-
-
-    # Energy range check
     if E is not None:
-        if E[0] >= E[1]:
-            raise ValueError("\nIn background_constant: invalid energy range\n")
-        small_s = small_s.inav.slice(E[0], E[1], 'energy')
-
-
-    # Average in roi = background
-    background_spectra = np.mean(small_s.data, axis=(0, 1))
-    background = np.mean(background_spectra)
+        s.isig[E[0]:E[1]] -= background
+    else:
+        s.data -= background
         
-    return s - background
-
+    return s
 
 def background_polyfit(s, x, y, E=None, order=1):
     """
@@ -65,7 +93,7 @@ def background_polyfit(s, x, y, E=None, order=1):
     y : tuple
         The range in the y-direction to consider for background calculation.
     E : tuple (optional)
-        The energy range to consider for background calculation.
+        The energy range to consider for background calculation in eV.
     order : int
         The order of the polynomial interpolation (default is 1 for linear).
 
@@ -75,46 +103,111 @@ def background_polyfit(s, x, y, E=None, order=1):
         The background-subtracted EELS spectrum.
     """
 
-    # Order check
-    if order < 0:
-        raise ValueError("\nIn background_interpolation: order must be non-negative\n")
-    if order==0:
-        print("\nIn background_interpolation: order 0 is equivalent to background_constant\n")
+    averaged_pixels, small_s = background_first_step(s, x, y, E)
+    
+    # Polynomial fit to the background spectrum
+    coeffs = np.polyfit(small_s.axes_manager[2].axis, averaged_pixels, order)
+    background = np.polyval(coeffs, small_s.axes_manager[2].axis)
+    
+    if E is not None:
+        s.isig[E[0]:E[1]] -= background
+    else:
+        s.data -= background
+
+    return s
+
+def background_interpolation(s, x, y, E=None):
+    """
+    Subtracts a background from EELS spectra by interpolating the signal.
+
+    Parameters:
+    -----------
+    s : hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The input (EELS) spectrum.
+    x : tuple
+        The range in the x-direction to consider for background calculation.
+    y : tuple
+        The range in the y-direction to consider for background calculation.
+    E : tuple (optional)
+        The energy range to consider for background calculation in eV.
+    order : int
+        The order of the polynomial interpolation (default is 1 for linear).
+
+    Returns:
+    --------
+    hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The background-subtracted EELS spectrum.
+    """
+
+    averaged_pixels, small_s = background_first_step(s, x, y, E)
+    
+    # Interpolation to the background spectrum
+    f = interp1d(small_s.axes_manager[2].axis, averaged_pixels, kind='cubic')
+    background = f(small_s.axes_manager[2].axis)
+
+    if E is not None:
+        s.isig[E[0]:E[1]] -= background
+    else:
+        s.data -= background
+
+    return s
+
+def background_removal(s, x, y, E=None, name='interpolation', order=None):
+    """
+    Subtracts background from EELS spectra using the specified method.
+
+    Parameters:
+    -----------
+    name : str
+        The method to use for background subtraction. Options are:
+        'constant' - constant background subtraction,
+        'polyfit' - polynomial fit background subtraction,
+        'interpolation' - interpolation background subtraction.
+    s : hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The input (EELS) spectrum.
+    x : tuple
+        The range in the x-direction to consider for background calculation.
+    y : tuple
+        The range in the y-direction to consider for background calculation.
+    E : tuple (optional)
+        The energy range to consider for background calculation in eV.
+    order : int (optional)
+        The order of the polynomial interpolation (only for 'polyfit')
+
+    Returns:
+    --------
+    hs.signals.EELSSpectrum or hs.signals.Signal1D
+        The background-subtracted EELS spectrum.
+    """
+
+    if name == 'constant':
         return background_constant(s, x, y, E)
     
+    if name == 'polyfit':
+        if order is None or order<1:
+            raise ValueError("\nIn background_subtraction: 'order' parameter must be specified correctly for 'polyfit' method\n")
+        if order == 0:
+            print("\nIn background_subtraction: 'order' parameter cannot be 0 for 'polyfit' method, switching to 'constant' method\n")
+            return background_constant(s, x, y, E)
+        return background_polyfit(s, x, y, E, order)
 
-    # Area check
-    if x[0] >= x[1] or y[0] >= y[1] or np.any(np.array(x) < 0) or np.any(np.array(y) < 0):
-        raise ValueError("\nIn background_interpolation: invalid ROI dimensions\n")
+    if name == 'interpolation':
+        return background_interpolation(s, x, y, E)
     
-    # Interpolated region: rectangular spatial ROI
-    roi = hs.roi.RectangularROI(left=x[0], right=x[1], top=y[0], bottom=y[1])
-    small_s = roi(s)
+    raise ValueError("\nIn background_subtraction: unknown method name\n")
 
 
-    # Energy range check
-    if E is not None:
-        if E[0] >= E[1]:
-            raise ValueError("\nIn background_interpolation: invalid energy range\n")
-        small_s = small_s.inav.slice(E[0], E[1], 'energy')
 
-    
-    # Interpolation process
-    # Pixels are averaged to get a single spectrum called background
-    averaged_pixels = np.mean(small_s.data, axis=(0, 1))
 
-    # Polynomial fit to the background spectrum
-    coeffs = np.polyfit(small_s.axes_manager[0].axis, averaged_pixels, order)
-    background = np.polyval(coeffs, s.axes_manager[0].axis)
-        
-
-    return s - background
 
 data_path_base = "C:/Users/lgarc/OneDrive/Escritorio/Universidad/Máster/TFM/Data/"
 data_path = data_path_base + 'Triangle/4-STEM SI.dm4'
 s = hs.load(data_path)[-1]
 
-background_polyfit(s, (0, 10), (0, 10), order=5).plot()
-plt.show(block=False)
+background_removal(s, (0, 10), (0, 10), E=(0.,3.)).plot()
+plt.show()
+
 
 input("Press Enter to continue...")
+
+
